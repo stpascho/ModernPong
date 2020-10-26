95e4220d23bd56c296b59dd525ccf12ce738f18d
stpascho <stephina.pascho@ucla.edu>
2020-10-26 21:02:07 +0000

    Added bounciness to ball and wall triggers

---
+++ Assets/BallController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public int randomNumber;
+    public float velocityX;
+    public float velocityY;
+    public Rigidbody2D rigidbodyBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodyBall = GetComponent<Rigidbody2D>();
+        randomNumber = Random.Range(2, 3);
+        if (randomNumber == 3)
+        {
+            rigidbodyBall.AddForce(new Vector2(80, 10));
+        }
+        else
+        {
+            rigidbodyBall.AddForce(new Vector2(-100, -10));
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D colInfo)
+    {
+
+        if (colInfo.collider.CompareTag("Player"))
+        {
+            velocityY = rigidbodyBall.velocity.y;
+            velocityX = rigidbodyBall.velocity.x;
+            rigidbodyBall.velocity = new Vector2(rigidbodyBall.velocity.x, velocityY / 2 + colInfo.collider.attachedRigidbody.velocity.y / 3);
+        }
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static int computerScore = 0; 
+    public static int playerScore = 0;
+
+    //public GUISkin skin;
+
+    public static void Score(string wallName)
+    {  //the "public tag" is important, otherwise you cant acces it form "SideWalls.cs"
+        if (wallName == "RightWall")
+        {
+            playerScore += 1;
+        }
+
+        else
+        {
+            computerScore += 1;
+            
+        }
+        Debug.Log("Player Score 01 is" + computerScore);
+        Debug.Log("Player Score 02 is" + playerScore);
+    }
+
+
+    void OnGUI()
+    {
+
+        GUI.Label(new Rect((Screen.width / 2) - 150, 25, 100, 100), "" + playerScore);
+        GUI.Label(new Rect(Screen.width / 2 + 150, 25, 100, 100), "" + computerScore);
+
+
+    }
+}
