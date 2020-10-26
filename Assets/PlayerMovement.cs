5523389839b19c64acf3eb2e497c573d5a933553
stpascho <stephina.pascho@ucla.edu>
2020-10-26 12:55:49 +0000

    Added player controller and rigidbodies

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public KeyCode moveUp = KeyCode.UpArrow;
+    public KeyCode moveDown = KeyCode.DownArrow;
+    public Rigidbody2D rigidbody2D;
+
+    public float speed = 10;
+
+    private void Awake()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(moveUp))
+        {
+            rigidbody2D.velocity = new Vector3(0, speed, 0);
+        }
+
+        else if (Input.GetKey(moveDown))
+        {
+            rigidbody2D.velocity = new Vector3(0, -1 * speed, 0);
+        }
+        else
+        {
+            rigidbody2D.velocity = new Vector3(0, 0, 0);
+        }
+
     }
 }
